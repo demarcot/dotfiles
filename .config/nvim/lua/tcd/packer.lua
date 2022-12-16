6b04338ce66db0158b02e18a790e04cd0faf7348
Thomas DeMarco <thomas.demarco@simplisafe.com>
2022-12-16 16:31:46 -0500

    feat(Tom): more copy. still something wrong with lsp

--- .config/nvim/lua/tcd/packer.lua
+++ .config/nvim/lua/tcd/packer.lua
@@ -1,4 +1,4 @@
-vim.cmd [[packadd packer.nvim]]
+vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
@@ -19,7 +19,7 @@ return require('packer').startup(function(use)
      end
  })
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
   -- use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
--- .config/nvim/lua/tcd/remap.lua
+++ .config/nvim/lua/tcd/remap.lua
@@ -1,4 +1,29 @@
 vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("n", "U", "<C-r>")
+
+-- Editor Nav
+vim.keymap.set("n", "<C-n>", "<C-d>zz")
+vim.keymap.set("n", "<C-p>", "<C-u>zz")
+
+-- Config Edits
+vim.keymap.set("n", "<leader><cr>", ":source ~/.config/nvim/init.lua<cr>")
+vim.keymap.set("n", "<leader>e<cr>", ":edit ~/.config/nvim/init.lua<cr>")
+vim.keymap.set("n", "<leader>ze<cr>", ":edit ~/.zshrc<cr>")
+
+-- File Nav
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files hidden=true<cr>")
+vim.keymap.set("n", "<leader>f", ":Ex<cr>")
+vim.keymap.set("n", "<leader>sf", ":Vex<cr>")
+
+
+-- Window/Buffer Nav
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+vim.keymap.set("n", "<leader>nb", ":bnext<cr>")
+vim.keymap.set("n", "<leader>pb", ":bprevious<cr>")
+vim.keymap.set("n", "<leader>nw", "<C-W>l")
+vim.keymap.set("n", "<leader>pw", "<C-W>h")
+vim.keymap.set("n", "<leader>ww", ":only<cr>")
+
 
 vim.keymap.set("n", "Q", "<nop>")
 
